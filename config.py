413a211f465929b88c60514e5394d434771efb07
AdamHawtin <adam.hawtin@ons.gov.uk>
2018-03-23 15:11:37 +0000

    Default to dev config for empty env

--- config.py
+++ config.py
@@ -2,14 +2,15 @@ import os
 
 
 class Config:
+    DEBUG = os.getenv('DEBUG', False)
     PORT = os.getenv('PORT')
     HOST = os.getenv('HOST')
 
 
 class DevelopmentConfig(Config):
+    DEBUG = os.getenv('DEBUG', True)
     PORT = os.getenv('PORT', 5000)
     HOST = os.getenv('HOST', '0.0.0.0')
-    DEBUG = True
 
 
 # TODO test config
--- run.py
+++ run.py
@@ -1,5 +1,9 @@
+import os
+
 from app.setup import create_app  # NOQA
 
+if not os.getenv('APP_SETTINGS'):
+    os.environ['APP_SETTINGS'] = 'DevelopmentConfig'
 
 app = create_app()
 
