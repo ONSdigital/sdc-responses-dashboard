1108686eb4b5e9f65a85b0a3b63695fb3820901e
Adam <35268982+AdamHawtin@users.noreply.github.com>
2018-03-23 15:21:18 +0000

    Redone app start up, added config (#3)

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+
+class Config:
+    DEBUG = os.getenv('DEBUG', False)
+    PORT = os.getenv('PORT')
+    HOST = os.getenv('HOST')
+
+
+class DevelopmentConfig(Config):
+    DEBUG = os.getenv('DEBUG', True)
+    PORT = os.getenv('PORT', 5000)
+    HOST = os.getenv('HOST', '0.0.0.0')
+
+
+# TODO test config
