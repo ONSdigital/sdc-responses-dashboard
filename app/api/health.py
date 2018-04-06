e2ce9b990b132f7db3455e22fe7c2dbd840ddf75
Ryan Grundy <ryan.grundy@ons.gov.uk>
2018-04-06 16:14:52 +0100

    Added root endpoint to health

--- app/api/health.py
+++ app/api/health.py
@@ -4,5 +4,6 @@ health_blueprint = Blueprint(name='health', import_name=__name__)
 
 
 @health_blueprint.route('/health')
+@health_blueprint.route('/')
 def health():
     return jsonify({'status': 'healthy'})
