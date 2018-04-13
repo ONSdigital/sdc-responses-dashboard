56c7bcce94e4c701015a1dce36c3083531cab54e
Adam <35268982+AdamHawtin@users.noreply.github.com>
2018-04-13 09:53:26 +0100

    JSON schema iteration (#8)
    * Update sample size mock to have minimum 100

--- app/api/report.py
+++ app/api/report.py
@@ -9,18 +9,20 @@ report_blueprint = Blueprint(name='reports', import_name=__name__)
 # Mocked endpoint until we have database query
 @report_blueprint.route('/report/<collection_exercise_id>', methods=['GET'])
 def get_report(collection_exercise_id):
-    sample_size = randint(0, 1000)
+    sample_size = randint(100, 1000)
     accounts_created = randint(0, sample_size)
     downloads = randint(0, accounts_created)
     uploads = randint(0, downloads)
 
     return jsonify({
-        'collectionExerciseId': collection_exercise_id,
-        'details': {
+        'metadata': {
+            'collectionExerciseId': collection_exercise_id,
+            'timeUpdated': int(datetime.now().timestamp())
+        },
+        'report': {
             'downloads': downloads,
             'uploads': uploads,
             'accountsCreated': accounts_created,
-            'sampleSize': sample_size,
-            'updated': int(datetime.now().timestamp())
+            'sampleSize': sample_size
         }
     })
