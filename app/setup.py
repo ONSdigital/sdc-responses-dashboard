b21bb9d902c2ae51e4a5f14be96616cb479c7eac
MebinAbraham <35296336+MebinAbraham@users.noreply.github.com>
2018-04-13 10:04:29 +0100

    Enabled CORS (#9)

--- app/setup.py
+++ app/setup.py
@@ -1,12 +1,14 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 
 import config
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app_config = getattr(config, os.getenv('APP_SETTINGS', 'Config'))
     app.config.from_object(app_config)
     add_blueprints(app)
