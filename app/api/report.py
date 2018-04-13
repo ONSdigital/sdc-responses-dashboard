3c94f360ec4dbd7afe50a5cdfa3671c8a0117eca
AdamHawtin <adam.hawtin@ons.gov.uk>
2018-04-13 08:58:14 +0100

    Update sample size mock to have minimum 100

--- app/api/report.py
+++ app/api/report.py
@@ -9,7 +9,7 @@ report_blueprint = Blueprint(name='reports', import_name=__name__)
 # Mocked endpoint until we have database query
 @report_blueprint.route('/report/<collection_exercise_id>', methods=['GET'])
 def get_report(collection_exercise_id):
-    sample_size = randint(0, 1000)
+    sample_size = randint(100, 1000)
     accounts_created = randint(0, sample_size)
     downloads = randint(0, accounts_created)
     uploads = randint(0, downloads)
