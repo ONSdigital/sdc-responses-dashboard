8d6e5e1905d9124fbf0bb9b097395cbd9905cd21
AdamHawtin <adam.hawtin@ons.gov.uk>
2018-05-04 10:56:45 +0100

    Added THREADED config variable, specify json content type

--- app/api/report.py
+++ app/api/report.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+import json
 from random import randint
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, Response
 
 report_blueprint = Blueprint(name='reports', import_name=__name__)
 
@@ -14,15 +15,17 @@ def get_report(collection_exercise_id):
     downloads = randint(0, accounts_created)
     uploads = randint(0, downloads)
 
-    return jsonify({
-        'metadata': {
-            'collectionExerciseId': collection_exercise_id,
-            'timeUpdated': int(datetime.now().timestamp())
-        },
-        'report': {
-            'downloads': downloads,
-            'uploads': uploads,
-            'accountsCreated': accounts_created,
-            'sampleSize': sample_size
-        }
-    })
+    return Response(
+        json.dumps({
+            'metadata': {
+                'collectionExerciseId': collection_exercise_id,
+                'timeUpdated': int(datetime.now().timestamp())
+            },
+            'report': {
+                'downloads': downloads,
+                'uploads': uploads,
+                'accountsCreated': accounts_created,
+                'sampleSize': sample_size
+            }
+        }),
+        content_type="Application/json")
--- run.py
+++ run.py
@@ -8,4 +8,7 @@ if not os.getenv('APP_SETTINGS'):
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host=app.config['HOST'], port=int(app.config['PORT']))
+    app.run(
+        host=app.config['HOST'],
+        port=int(app.config['PORT']),
+        threaded=app.config['THREADED'])
