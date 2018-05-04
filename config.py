5197b9e3a91006c1e927b78df4c77caa6581e8eb
AdamHawtin <adam.hawtin@ons.gov.uk>
2018-05-04 15:13:40 +0100

    Set the flask built in environment config

--- config.py
+++ config.py
@@ -5,15 +5,16 @@ class Config:
     DEBUG = os.getenv('DEBUG', False)
     PORT = os.getenv('PORT')
     HOST = os.getenv('HOST')
-    THREADED = os.getenv('THREADED', True)
 
 
 class DevelopmentConfig(Config):
     DEBUG = os.getenv('DEBUG', True)
     PORT = os.getenv('PORT', 5000)
     HOST = os.getenv('HOST', 'localhost')
+    ENV = os.getenv('FLASK_ENV', 'development')
 
 
 class TestingConfig(Config):
     PORT = os.getenv('PORT', 5000)
     HOST = os.getenv('HOST', 'localhost')
+    TESTING = True
