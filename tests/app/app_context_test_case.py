5fbbeb8329ac5a0236998f2ac56c92b87e351994
Mebin Abraham <mebin.abraham@qa.com>
2018-05-04 14:45:27 +0100

    Added basic unit tests

---
+++ tests/app/app_context_test_case.py
@@ -0,0 +1,12 @@
+import unittest
+
+from app.setup import create_app
+from config import TestingConfig
+
+
+class AppContextTestCase(unittest.TestCase):
+
+    def setUp(self):
+        app = create_app()
+        app.config.from_object(TestingConfig)
+        self.test_client = app.test_client()
