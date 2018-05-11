8ed50e5c9d1ce036b44709aa59c1f25e762d0c3c
Adam <35268982+AdamHawtin@users.noreply.github.com>
2018-05-11 16:14:04 +0100

    Set the flask built in environment config (#13)

--- run.py
+++ run.py
@@ -10,5 +10,4 @@ app = create_app()
 if __name__ == '__main__':
     app.run(
         host=app.config['HOST'],
-        port=int(app.config['PORT']),
-        threaded=app.config['THREADED'])
+        port=int(app.config['PORT']))
