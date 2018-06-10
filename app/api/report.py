bb1ed86c542313fa655a830cb1bd8189b6cf7a78
Mebin Abraham <mebin.abraham@qa.com>
2018-06-10 18:31:19 +0100

    Bootstrap version

--- app/api/report.py
+++ app/api/report.py
@@ -8,7 +8,7 @@ report_blueprint = Blueprint(name='reports', import_name=__name__)
 
 
 # Mocked endpoint until we have database query
-@report_blueprint.route('/report/<collection_exercise_id>', methods=['GET'])
+@report_blueprint.route('/report/collection-exercise/<collection_exercise_id>', methods=['GET'])
 def get_report(collection_exercise_id):
     sample_size = randint(100, 1000)
     accounts_created = randint(0, sample_size)
@@ -24,7 +24,7 @@ def get_report(collection_exercise_id):
             'report': {
                 'downloads': downloads,
                 'uploads': uploads,
-                'accountsCreated': accounts_created,
+                'accountsEnrolled': accounts_created,
                 'sampleSize': sample_size
             }
         }),
--- app/api/survey.py
+++ app/api/survey.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template
 
-survey_blueprint = Blueprint(name='index', import_name=__name__)
+survey_blueprint = Blueprint(name='survey', import_name=__name__)
 
 
-@survey_blueprint.route('/survey/<collection_exercise_id>', methods=['GET'])
+@survey_blueprint.route('/survey/collection-exercise/<collection_exercise_id>', methods=['GET'])
 def get_survey(collection_exercise_id):
     return render_template('survey.html', collex_id=collection_exercise_id)
