d2e3ff4bb7d9022c19303a298326e291db5b6ab8
AdamHawtin <adam.hawtin@ons.gov.uk>
2018-06-21 16:23:54 +0100

    Remove debugging print lines

--- app/api/survey.py
+++ app/api/survey.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 from flask import Blueprint, render_template, current_app, abort
 
 from app.api.nocache import nocache
@@ -11,17 +9,12 @@ survey_blueprint = Blueprint(name='survey', import_name=__name__)
 @survey_blueprint.route('/survey/collection-exercise/<collection_exercise_id>', methods=['GET'])
 @nocache
 def get_survey(collection_exercise_id):
-
     surveys_metadata, collection_exercise_metadata = fetch_survey_and_collection_exercise_metadata()
 
     try:
         collection_exercise_name = collection_exercise_metadata[collection_exercise_id]['collexName']
     except KeyError:
         abort(404)
-
-    pprint(surveys_metadata)
-    print(collection_exercise_id)
-    print(collection_exercise_name)
 
     return render_template(
         'survey.html',
