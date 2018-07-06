6c821b69197c9603c87a7bb1531a85d801008ded
MebinAbraham <35296336+MebinAbraham@users.noreply.github.com>
2018-07-06 14:42:54 +0100

    Select collection exercise based on survey (#22)
    Show collection exercise based on the chosen survey.
    Only displays surveys with at least one collection exercise.

--- app/static/assets/js/dashboard.js
+++ app/static/assets/js/dashboard.js
@@ -1,72 +1,138 @@
 /*jshint esversion: 6 */
 function callAPI(collexID = $("#collex-id").data("collex"), enableTimeout = true) {
+    const reportingURL = $("#collex-id").data("reporting-url");
+    const reportingRefreshCycle = $("#collex-id").data("reporting-refresh-cycle");
 
-	const reportingURL = $("#collex-id").data("reporting-url");
-	const reportingRefreshCycle = $("#collex-id").data("reporting-refresh-cycle");
+    $.ajax({
+        dataType: "json",
+        url: reportingURL + "reporting-api/v1/response-dashboard/collection_exercise/" + collexID
+    }).done((result) => {
+        const downloads = result.report.downloads;
+        const uploads = result.report.uploads;
+        const accountsEnrolled = result.report.accountsEnrolled;
+        const sampleSize = result.report.sampleSize;
+        const timeUpdated = moment.unix(result.metadata.timeUpdated).calendar(); // eslint-disable-line
+        const progress = (uploads / sampleSize * 100).toFixed(2);
 
-	$.ajax({
-		url: reportingURL + "reporting-api/v1/response-dashboard/collection_exercise/" + collexID
-	}).done((result) => {
-		const downloads = result.report.downloads;
-		const uploads = result.report.uploads;
-		const accountsEnrolled = result.report.accountsEnrolled;
-		const sampleSize = result.report.sampleSize;
-		const timeUpdated = moment.unix(result.metadata.timeUpdated).calendar(); // eslint-disable-line
-		const progress = (uploads / sampleSize * 100).toFixed(2);
+        $('#error-reporting').hide();
+        $('.content').show();
 
-		$("#downloads-counter").text(downloads).effect("bounce", "slow");
-		$("#uploads-counter").text(uploads).effect("bounce", "slow");
-		$("#accounts-enrolled-counter").text(accountsEnrolled).effect("bounce", "slow");
-		$("#sample-size-counter").text(sampleSize).effect("bounce", "slow");
-		$("#time-updated").text(timeUpdated);
-		$("#progress-uploaded").text(uploads);
-		$("#progress-size").text(sampleSize);
-		$("#collex-progress").text(progress + "%").css("width", progress + "%");
+        $("#downloads-counter").text(downloads).effect("bounce", "slow");
+        $("#uploads-counter").text(uploads).effect("bounce", "slow");
+        $("#accounts-enrolled-counter").text(accountsEnrolled).effect("bounce", "slow");
+        $("#sample-size-counter").text(sampleSize).effect("bounce", "slow");
+        $("#time-updated").text(timeUpdated);
+        $("#progress-uploaded").text(uploads);
+        $("#progress-size").text(sampleSize);
+        $("#collex-progress").text(progress + "%").css("width", progress + "%");
 
-		if (enableTimeout) {
-			setTimeout(callAPI, reportingRefreshCycle);
-		}
-	});
+        if (enableTimeout) {
+            setTimeout(callAPI, reportingRefreshCycle);
+        }
+    }).fail((result) => {
+        $('#error-reporting').show();
+        $('.content').hide();
+
+        if (enableTimeout) {
+            setTimeout(callAPI, reportingRefreshCycle);
+        }
+    });
 }
 
-function loadDataTable() {
+function initiliseDataTables() {
 
-	/* eslint-disable */
-	let dTable = $("#surveys").DataTable({ 
-		"paging": true,
-		"lengthChange": false,
-		"searching": true,
-		"ordering": true,
-		"info": true,
-		"autoWidth": false,
-		"autoHeight": false,
-		"scrollY": 300,
-		"scroller": {
-			loadingIndicator: false
-		}
-	});
-	/* eslint-enable */
+    /* eslint-disable */
+    const surveyTable = $("#survey-datatable").DataTable({
+        paging: true,
+        lengthChange: false,
+        searching: true,
+        ordering: true,
+        info: true,
+        autoWidth: false,
+        autoHeight: false,
+        scrollY: "30vh",
+        scrollCollapse: true,
+        scroller: {
+            loadingIndicator: false
+        }
+    });
+    /* eslint-enable */
 
-	$("#survey-search").keyup(function() {
-		dTable.search($(this).val()).draw();
-	});
+    $("#survey-search").keyup(function() {
+        surveyTable.search($(this).val()).draw();
+    });
 
-	$("#surveys tbody").on("click", "tr", function() {
-		const surveyLongName = $(this).data("survey-long-name");
-		const surveyShortName = dTable.row(this).data()[0];
-		const collectionExercise = dTable.row(this).data()[2];
-		const id = dTable.row(this).id();
+    /* eslint-disable */
+    const collexTable = $("#collex-datatable").DataTable({
+        paging: true,
+        lengthChange: false,
+        searching: true,
+        ordering: true,
+        info: true,
+        autoWidth: false,
+        autoHeight: false,
+        scrollY: "30vh",
+        scrollCollapse: true,
+        scroller: {
+            loadingIndicator: false
+        },
+        data: [],
+        columns: [{
+            "data": "userDescription",
+            "title": "Collection Exercise Period",
+            "width": "600px"
+        }],
+        rowId: 'collectionExerciseId'
+    });
+    /* eslint-enable */
 
-		$("#collex-id").data("collex", id);
-		$("#survey-short-name").text(surveyShortName + " | " + collectionExercise);
-		$("#survey-long-name").text(surveyLongName);
-		$("#modal-default").modal("toggle");
+    $("#collex-search").keyup(function() {
+        collexTable.search($(this).val()).draw();
+    });
 
-		callAPI(id, false);
-	});
+    $("#survey-datatable tbody").on("click", "tr", function() {
+        const id = surveyTable.row(this).id();
+
+        if (typeof id !== "undefined") {
+            const surveyShortName = $(this).data("survey-short-name");
+
+            $("#chosen-survey").text(surveyShortName);
+			$("#modal-survey").modal("toggle");
+			
+            loadCollexTableData(collexTable, id);
+        }
+
+    });
+
+}
+
+function loadCollexTableData(collexTable, id) {
+    const surveys = JSON.parse($('#collex-id').data('surveys'));
+
+    collexTable.clear().draw();
+    collexTable.rows.add(get_collex_from_survey_id(surveys, id)).draw();
+
+    $("#modal-collex").modal("toggle");
+    $("#collex-datatable tbody").on("click", "tr", function() {
+        const id = collexTable.row(this).id();
+
+        if (typeof id !== "undefined") {
+            window.location.href = id;
+        }
+    });
+}
+
+function get_collex_from_survey_id(surveys, survey_id) {
+
+    for (let i = 0; i < surveys.length; i++) {
+        if (surveys[i].surveyId === survey_id) {
+            collection_exercises = surveys[i].collectionExercises;
+            return collection_exercises;
+        }
+    }
 }
 
 $(document).ready(function() {
-	callAPI();
-	loadDataTable();
+    callAPI();
+    initiliseDataTables();
 });
