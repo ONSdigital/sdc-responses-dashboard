35dc2c49a1baa0d26fbfc5528eb9101c04076cf1
Ryan Grundy <ryan.grundy@ons.gov.uk>
2018-08-17 15:31:27 +0100

    Fixed 500 and 404 error handling

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -7,26 +7,22 @@ logger = get_logger()
 def not_found_error(error):
     custom_error_msg = 'Sorry, we could not find the page you were looking for.'
 
-    logger.error(error.name,
-                 error=error,
-                 status_code=error.code,
-                 description=error.description)
+    logger.error("Handling 404 error",
+                 error=error)
 
     return render_template('errors/error.html',
-                           status_code=error.code,
-                           error_name=error.name,
+                           status_code=404,
+                           error_name='Not found error',
                            error_msg=custom_error_msg), 404
 
 
 def internal_server_error(error):
     custom_error_msg = 'Sorry, something has gone wrong.'
 
-    logger.error(error.name,
-                 error=error,
-                 status_code=error.code,
-                 description=error.description)
+    logger.error("Handling 500 error",
+                 error=error)
 
     return render_template('errors/error.html',
-                           status_code=error.code,
-                           error_name=error.name,
+                           status_code=500,
+                           error_name='Internal server error',
                            error_msg=custom_error_msg), 500
