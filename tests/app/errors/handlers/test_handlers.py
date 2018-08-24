3d5481d2ed4fa28bbb82f9756ecce2343a83bb6d
ryangrundy7 <35301276+ryangrundy7@users.noreply.github.com>
2018-08-24 10:36:19 +0100

    Fixed error handling (#36)
    Fixed error handlers in the dashboard

--- tests/app/errors/handlers/test_handlers.py
+++ tests/app/errors/handlers/test_handlers.py
@@ -8,8 +8,7 @@ class TestErrorHandlers(AppContextTestCase):
     def test_not_found_error(self):
         response = self.test_client.get('/nonexistent-endpoint')
         self.assertEqual(response.status_code, 404)
-        self.assertEqual(response.status, '404 NOT FOUND')
-        self.assertIn(b'Not Found', response.data, )
+        self.assertIn(b'Oops! Not found error', response.data, )
         self.assertIn(b'Sorry, we could not find the page you were looking for.', response.data)
 
     @responses.activate
@@ -24,7 +23,6 @@ class TestErrorHandlers(AppContextTestCase):
         response = self.test_client.get('/collection-exercise/00000000-0000-0000-0000-000000000000')
 
         self.assertEqual(response.status_code, 500)
-        self.assertEqual(response.status, '500 INTERNAL SERVER ERROR')
-        self.assertIn(b'Internal Server Error', response.data, )
+        self.assertIn(b'Oops! Internal server error', response.data, )
         self.assertIn(b'Sorry, something has gone wrong.', response.data)
 
