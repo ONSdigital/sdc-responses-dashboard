0b7403bce9e7e6825722e7c7dcfc1d201db19ad1
MebinAbraham <35296336+MebinAbraham@users.noreply.github.com>
2018-11-16 10:12:52 +0000

    Specify reporting refresh cycle in seconds (#53)
    Specify reporting cycle in seconds

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -40,7 +40,7 @@ def get_survey_details(collection_exercise_id):
         survey_short_name=collection_exercise['shortName'],
         survey_long_name=collection_exercise['longName'],
         collection_exercise_description=collection_exercise['userDescription'],
-        reporting_refresh_cycle=int(current_app.config['REPORTING_REFRESH_CYCLE'])
+        reporting_refresh_cycle=int(current_app.config['REPORTING_REFRESH_CYCLE_IN_SECONDS'])
     )
 
 
