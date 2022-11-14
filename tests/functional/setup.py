787bedefdc72d49d9d3ad5819f39e2176704506e
Adam Mann <manny160389@hotmail.com>
2022-11-14 11:34:37 +0000

    Upgrade python to 3.11 and upgrade action versions (#131)
    * Upgrade python to 3.11 and upgrade action versions
    
    * Unpin black.  Comment out pipenv check for now
    
    * Remove chromedriver binary to let tests run
    
    * Uncomment pipenv check and add exception

--- tests/functional/setup.py
+++ tests/functional/setup.py
@@ -1,12 +1,10 @@
 import os
 from distutils.util import strtobool
 
-import chromedriver_binary
 from selenium import webdriver
 
 
 def create_browser():
-    chromedriver_binary.add_chromedriver_to_path()
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--no-sandbox")
     chrome_options.headless = bool(strtobool(os.getenv("HEADLESS", "True")))
